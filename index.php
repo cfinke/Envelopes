2ab2fddab797df731b6bcaead71c65177c6dfa69
Christopher Finke <cfinke@gmail.com>
2020-12-12 04:58:33 +0000

    A bunch of changes I forgot to commit individually. :)

--- index.php
+++ index.php
@@ -56,8 +56,6 @@ if ( isset( $_POST['envelopes_submit'] ) ) {
 	while ( ! feof( $file ) && $line = fgetcsv( $file ) ) {
 		$array_for_this_line = array();
 		
-		$line = array_map( 'stripslashes_deep', $line );
-				
 		foreach ( $line as $idx => $value ) {
 			$column_index = $idx;
 			$column_name = $header_line[ $column_index ];
@@ -69,37 +67,38 @@ if ( isset( $_POST['envelopes_submit'] ) ) {
 	}
 	
 	fclose( $file );
+
+	$margin = 10;
 	
+	$return_address_line_height = max( 1, intval( $_POST['envelopes_return_address_font_size'] ) );
+	$return_address_line_padding = 2;
+	
+	$to_address_line_height = max( 1, intval( $_POST['envelopes_to_address_font_size'] ) );
+
 	foreach ( $csv_as_array as $entry ) {
 		$pdf->SetPrintHeader(false);
 		$pdf->SetPrintFooter(false);
 		$pdf->AddPage();
-		
+
+		// Add return address.
+		$x = $margin;
+		$y = $margin;
 
 		$address = $_POST['envelopes_format'];
 
 		foreach ( $entry as $column => $value ) {
-			$address = str_replace( '%' . $column . '%', trim( $value ), $address );
+			$address = str_replace( '{{' . $column . '}}', trim( $value ), $address );
 		}
-		
+
 		$address = preg_replace( '/\n(\s*)\n+/', "\n", $address );
 		$address = trim( $address );
 		
-		$margin = 10;
-		
-		// Add return address.
-		$x = $margin;
-		$y = $margin;
-		
-		$line_height = 12;
-		$line_padding = 2;
-
-		if ( ! empty( $_FILES['envelopes_image'] ) ) {
+		if ( ! empty( $_FILES['envelopes_image']['size'] ) ) {
 			$pdf->SetXY( $x, $y );
 			
 			$image_size = getimagesize( $_FILES['envelopes_image']['tmp_name'] );
 			
-			$height = ( $line_height * 3 ) + ( $line_padding * 2 );
+			$height = ( $return_address_line_height * 3 ) + ( $return_address_line_padding * 2 );
 			$width = $height * ( $image_size[0] / $image_size[1] );
 			
 			$pdf->Image( $_FILES['envelopes_image']['tmp_name'], $x, $y, $width, $height );
@@ -107,17 +106,22 @@ if ( isset( $_POST['envelopes_submit'] ) ) {
 		}
 
 		$pdf->SetXY( $x, $y );
-		$pdf->SetFont( 'dejavusans', '', $line_height );
-		$pdf->MultiCell( $page_width * .6, $line_height, $_POST['envelopes_return_address'], 0, "L" );
-
+		$pdf->SetFont( 'dejavusans', '', $return_address_line_height );
+		$pdf->MultiCell( $page_width, $return_address_line_height, $_POST['envelopes_return_address'], 0, "L" );
 
 		// Location for the to: address.
-		$x = $page_width * 0.40;
-		$y = $page_height * 0.50;
+		if ( 'P' == $orientation ) {
+			$x = $page_width * 0.30;
+		}
+		else {
+			$x = $page_width * 0.4;
+		}
+
+		$y = $page_height * 0.45;
 		
 		$pdf->SetXY( $x, $y );
-		
-		$pdf->MultiCell( $page_width / 2, $line_height, $address, 0, "L" );
+		$pdf->SetFont( 'dejavusans', '', $to_address_line_height );
+		$pdf->MultiCell( $page_width / 2, $to_address_line_height, $address, 0, "L" );
 	}
 	
 	$pdf->Output();
@@ -170,6 +174,14 @@ if ( isset( $_POST['envelopes_submit'] ) ) {
 					</tr>
 					<tr>
 						<th scope="row">
+							<label for="envelopes_return_address_font_size">Return Address Font Size</label>
+						</th>
+						<td>
+							<input type="number" name="envelopes_return_address_font_size" value="12" />
+						</td>
+					</tr>
+					<tr>
+						<th scope="row">
 							<label for="envelopes_image">Return Address Image</label>
 						</th>
 						<td>
@@ -180,7 +192,7 @@ if ( isset( $_POST['envelopes_submit'] ) ) {
 						<th scope="row">
 							<label for="envelopes_format">Format</label>
 							<p>Example:</p>
-							<p><code>%First Name% %Last Name%<br />%Address%<br />%City%, %State% %ZIP%</code></p>
+							<p><code>{{First Name}} {{Last Name}}<br />{{Address}}<br />{{City}}, {{State}} {{Zip}}</code></p>
 						</th>
 						<td>
 							<textarea name="envelopes_format" id="envelopes_format"></textarea>
@@ -192,6 +204,14 @@ if ( isset( $_POST['envelopes_submit'] ) ) {
 						</th>
 						<td>
 							<input type="file" id="envelopes_csv" name="envelopes_csv" />
+						</td>
+					</tr>
+					<tr>
+						<th scope="row">
+							<label for="envelopes_to_address_font_size">To: Address Font Size</label>
+						</th>
+						<td>
+							<input type="number" name="envelopes_to_address_font_size" value="12" />
 						</td>
 					</tr>
 				</tbody>
